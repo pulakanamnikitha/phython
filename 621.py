9290421b53a63ef0553d33988799adae8c2bca52
pulakanamnikitha <42571494+pulakanamnikitha@users.noreply.github.com>
2018-12-07 12:43:43 +0530

    Update and rename 62.py to 621.py

--- 621.py
+++ 621.py
@@ -1,10 +1,10 @@
-n=raw_input()
-"""if all(c in '01' for c in n):
+q=raw_input()
+"""if all(c in '01' for c in q):
     print "Yes"
 else:
     print "No"
     """
-if not(n.translate(None,'01')):
+if not(q.translate(None,'01')):
     print "Yes"
 else:
     print "No"
