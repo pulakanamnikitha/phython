69331a6622434605daaedd36d8c119fdf6b53a6a
pulakanamnikitha <42571494+pulakanamnikitha@users.noreply.github.com>
2019-01-04 20:55:49 +0530

    Update and rename norepeat.py to norepeat1.py

--- norepeat1.py
+++ norepeat1.py
@@ -1,10 +1,10 @@
 def no_repeated_letters(word_1):
     lettersCount = {}
-    for i in word_1:
-        if i not in lettersCount:
-            lettersCount[i] = 1
+    for n in word_1:
+        if n not in lettersCount:
+            lettersCount[n] = 1
         else:
-            lettersCount[i] += 1
+            lettersCount[n] += 1
 
     return max(lettersCount, key=lettersCount.get)
 
